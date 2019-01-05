3911440382607060c1d342320aac31bc3fa01cde
xiaohui <1379593215@qq.com>
2019-01-05 14:13:01 +0800

    'vue2.5'

---
+++ Todolist/src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li @click="handleClick">{{content}}</li>
+</template>
+
+<script>
+export default {
+    props: ['content','index'],
+    methods: {
+        handleClick:function(){
+            this.$emit('delete',this.index)
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!--全局样式与局部样式-->
+<style scoped>
+
+</style>
